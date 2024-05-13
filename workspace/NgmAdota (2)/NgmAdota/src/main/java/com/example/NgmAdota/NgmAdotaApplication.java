790ec0d25626e3ace940e336b01a08aba10e0b95
ArthurTorres1 <arthurtorres059@gmail.com>
2024-05-13 10:29:41 -0300

    adicionando pasta workspace, com a conexão COM O SQL SERVER

---
+++ workspace/NgmAdota (2)/NgmAdota/src/main/java/com/example/NgmAdota/NgmAdotaApplication.java
@@ -0,0 +1,13 @@
+package com.example.NgmAdota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgmAdotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgmAdotaApplication.class, args);
+	}
+
+}
---
+++ workspace/NgmAdota (2)/NgmAdota/src/test/java/com/example/NgmAdota/NgmAdotaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NgmAdota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgmAdotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
