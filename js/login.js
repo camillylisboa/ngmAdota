8ed2765ebbb83c1e0ad8a0a4f5bbe72bd77d0038
ArthurTorres1 <arthurtorres059@gmail.com>
2024-05-17 10:15:04 -0300

    Chamando o nome do usuário através do login.

--- js/login.js
+++ js/login.js
@@ -1,23 +1,25 @@
 function login() {
     var requestAutenticacao = {
-        "email" : $("#txt_email").val(),
-        "senha" : $("#txt_senha").val()
-    }
+        "email": $("#txt_email").val(),
+        "senha": $("#txt_senha").val()
+    };
 
     $.ajax({
-        url : "http://localhost:8080/auth/login",
-        type : "POST",
-        data : JSON.stringify(requestAutenticacao),
-        contentType : "application/json",
-        dataType : "json",
-        success : function (response){
+        url: "http://localhost:8080/auth/login",
+        type: "POST",
+        data: JSON.stringify(requestAutenticacao),
+        contentType: "application/json",
+        dataType: "json",
+        success: function (response) {
             var token = response.token;
+            var nome = response.nome;
             window.localStorage.setItem('token', token);
+            window.localStorage.setItem('nomeUsuario', nome);
             window.location.href = './index.html';
-            alert("login feito com sucesso");
+            alert("Login feito com sucesso");
         },
-        error: function(request, message, error){
-            alert("Erro ao se autenticar")
+        error: function (request, message, error) {
+            alert("Erro ao se autenticar");
         }
-    })
+    });
 }
