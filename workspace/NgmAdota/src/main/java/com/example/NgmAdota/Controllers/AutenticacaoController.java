7c05bc1f15b9b69b65247a899e49dd399fbcb94e
ArthurTorres1 <arthurtorres059@gmail.com>
2024-05-23 10:59:20 -0300

    Criando PUT de usuario

--- workspace/NgmAdota/src/main/java/com/example/NgmAdota/Controllers/AutenticacaoController.java
+++ workspace/NgmAdota/src/main/java/com/example/NgmAdota/Controllers/AutenticacaoController.java
@@ -1,5 +1,6 @@
 package com.example.NgmAdota.Controllers;
 
+import com.example.NgmAdota.Dtos.AnimalRequestDTO;
 import com.example.NgmAdota.Dtos.CadastroRequestDTO;
 import com.example.NgmAdota.Dtos.LoginRequestDTO;
 import com.example.NgmAdota.Dtos.ResponseUsuarioDTO;
@@ -8,6 +9,7 @@ import com.example.NgmAdota.Repositories.UsuarioRepository;
 import com.example.NgmAdota.infra.security.TokenService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
@@ -22,6 +24,24 @@ public class AutenticacaoController {
     private final UsuarioRepository usuarioRepository;
     private final PasswordEncoder passwordEncoder;
     private final TokenService tokenService;
+
+    @PutMapping("/editar/{id}")
+    public ResponseEntity<Object> editarUsuario(@PathVariable(value = "id") Long id,
+                                                @RequestBody @Valid CadastroRequestDTO request){
+        Optional<UsuarioModel> usuarioEdit = usuarioRepository.findById(id);
+        if(usuarioEdit.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Não foi possivel editar os dados deste animal, pois ele não foi encontrado");
+        }
+        var usuarioModel = usuarioEdit.get();
+        usuarioModel.setNome(request.nome());
+        usuarioModel.setEmail(request.email());
+        usuarioModel.setDataNascimento(request.dataNascimento());
+        usuarioModel.setTelefone(request.telefone());
+        usuarioModel.setSenha(request.senha());
+        this.usuarioRepository.save(usuarioModel);
+        return ResponseEntity.ok(new ResponseUsuarioDTO(usuarioModel.getNome(), usuarioModel.getEmail(),
+                                usuarioModel.getTelefone(), usuarioModel.idade(), usuarioModel.getSenha()));
+    }
 
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody @Valid LoginRequestDTO request){
