9280577d696f87e23a381710b826e548a46121f4
ArthurTorres1 <arthurtorres059@gmail.com>
2024-08-18 19:50:02 -0300

    Rota de cadastro de animal funcionando e role ONG cadastrando animais

--- workspace/NgmAdota/src/main/java/com/example/NgmAdota/modules/ong/services/CreateAnimalService.java
+++ workspace/NgmAdota/src/main/java/com/example/NgmAdota/modules/ong/services/CreateAnimalService.java
@@ -1,9 +1,11 @@
 package com.example.NgmAdota.modules.ong.services;
 
 import com.example.NgmAdota.exceptions.AnimalFoundException;
+import com.example.NgmAdota.exceptions.OngNotFoundException;
 import com.example.NgmAdota.exceptions.UserFoundException;
 import com.example.NgmAdota.modules.ong.AnimalModel;
 import com.example.NgmAdota.modules.ong.AnimalRepository;
+import com.example.NgmAdota.modules.ong.OngRepository;
 import com.example.NgmAdota.modules.ong.dto.RequestAnimalDTO;
 import com.example.NgmAdota.modules.ong.dto.ResponseAnimalDTO;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,53 +21,22 @@ public class CreateAnimalService {
     @Autowired
     private AnimalRepository animalRepository;
 
-    public ResponseAnimalDTO execute(@Valid RequestAnimalDTO animalDTO) {
+    @Autowired
+    private OngRepository ongRepository;
+
+    public AnimalModel execute(@Valid AnimalModel animalModel) {
         // Verificar se o animal já existe (opcional)
         animalRepository.findByNomeAndImagemAndDataNascimentoAndDescricao(
-                animalDTO.nome(),
-                animalDTO.imagem(),
-                animalDTO.dataNascimento(),
-                animalDTO.descricao()
+                animalModel.getNome(),
+                animalModel.getImagem(),
+                animalModel.getDataNascimento(),
+                animalModel.getDescricao()
         ).ifPresent(animal -> {
             throw new AnimalFoundException("Animal já existe em nossa base de dados");
         });
-
-        // Converter DTO para Entidade
-        AnimalModel animal = convertToEntity(animalDTO);
-
-        // Salvar a entidade
-        AnimalModel savedAnimal = animalRepository.save(animal);
-
-        // Converter Entidade para DTO de Resposta
-        return convertToResponseDTO(savedAnimal);
-    }
-
-    private AnimalModel convertToEntity(RequestAnimalDTO animalDTO) {
-        AnimalModel animal = new AnimalModel();
-        animal.setNome(animalDTO.nome());
-        animal.setImagem(animalDTO.imagem());
-        animal.setDataNascimento(animalDTO.dataNascimento());
-        animal.setPeso(animalDTO.peso());
-        animal.setIdade(animal.getIdade());
-        animal.setSexo(animalDTO.sexo());
-        animal.setDescricao(animalDTO.descricao());
-        animal.setIdRaca(animalDTO.idRaca());
-        animal.setIdEspecie(animalDTO.idEspecie());
-        animal.setIdPelagem(animalDTO.idPelagem());
-        animal.setIdPorte(animalDTO.idPorte());
-        return animal;
-    }
-
-    private ResponseAnimalDTO convertToResponseDTO(AnimalModel animal) {
-        int idade = Period.between(animal.getDataNascimento(), LocalDate.now()).getYears();
-        return ResponseAnimalDTO.builder()
-                .id(animal.getId())
-                .nome(animal.getNome())
-                .imagem(animal.getImagem())
-                .descricao(animal.getDescricao())
-                .sexo(animal.getSexo())
-                .idade(idade)
-                .peso(animal.getPeso())
-                .build();
+        if(this.ongRepository.findById(animalModel.getOngId()).isPresent()){
+            return this.animalRepository.save(animalModel);
+        }
+        throw new OngNotFoundException();
     }
 }
