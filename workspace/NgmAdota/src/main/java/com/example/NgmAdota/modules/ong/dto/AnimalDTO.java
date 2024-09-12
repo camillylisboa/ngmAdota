c4d30a5866bd548ec6c8cd03d9ea232b1ae8c981
Enzo Basile <basileenzo22@gmail.com>
2024-09-12 10:08:52 -0300

    favorito pronto

---
+++ workspace/NgmAdota/src/main/java/com/example/NgmAdota/modules/ong/dto/AnimalDTO.java
@@ -0,0 +1,2 @@
+package com.example.NgmAdota.modules.ong.dto;public record AnimalDTO() {
+}
--- workspace/NgmAdota/src/main/java/com/example/NgmAdota/modules/ong/dto/FavoritoDTO.java
+++ workspace/NgmAdota/src/main/java/com/example/NgmAdota/modules/ong/dto/FavoritoDTO.java
@@ -1,10 +1,2 @@
-package com.example.NgmAdota.modules.ong.dto;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
-public class FavoritoDTO {
-    private boolean favorito;
+package com.example.NgmAdota.modules.ong.dto;public record FavoritoDTO() {
 }
