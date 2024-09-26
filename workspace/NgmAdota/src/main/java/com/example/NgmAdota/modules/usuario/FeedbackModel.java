30b8efccee1f0eba18d3ddbb390339f67e1169ac
Enzo Basile <basileenzo22@gmail.com>
2024-09-26 11:06:18 -0300

    feedback

--- workspace/NgmAdota/src/main/java/com/example/NgmAdota/modules/usuario/FeedbackModel.java
+++ workspace/NgmAdota/src/main/java/com/example/NgmAdota/modules/usuario/FeedbackModel.java
@@ -1,2 +1,38 @@
-package com.example.NgmAdota.modules.usuario;public class FeedbackModel {
+package com.example.NgmAdota.modules.usuario;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tabFeedback")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class FeedbackModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String comentario;
+
+    @ManyToOne()
+    @JoinColumn(name = "usuario_id", updatable = false,  insertable = false)
+    private UsuarioModel usuarioModel;
+
+    @Column(name = "usuario_id", nullable = false)
+    private Long usuarioId;
+
+    private Integer estrelas;
+
+    @CreationTimestamp
+    private LocalDateTime dataFeedback;
 }
--- workspace/NgmAdota/src/main/java/com/example/NgmAdota/modules/usuario/FeedbackRepository.java
+++ workspace/NgmAdota/src/main/java/com/example/NgmAdota/modules/usuario/FeedbackRepository.java
@@ -1,2 +1,9 @@
-package com.example.NgmAdota.modules.usuario;public interface FeedbackRepository {
+package com.example.NgmAdota.modules.usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FeedbackRepository extends JpaRepository<FeedbackModel, Long> {
+
 }
